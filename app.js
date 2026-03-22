b7b50567f1ef78b84bccb477d9eb8338a6a775f6
TANGHuyming <2023277tang@aupp.edu.kh>
2026-03-22 14:56:20 +0700

    add: simple structure

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const {engine} = require('express-handlebars')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+// configurations for app
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials',
+    helpers: {
+        section: function(name, options) {
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        }
+    }
+}))
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
+
+// middleware
+app.use(express.static(__dirname + '/public'))
+
+// routes
+app.get('/items', (req, res) => {
+    const context = {
+        categories: [
+            {id: 1, name: 'category1'},
+            {id: 2, name: 'category2'},
+            {id: 3, name: 'category3'}
+        ],
+        items: [
+            {id: 1, name: 'item 1', description: 'item 1 description', imagePath: '/images/placeholder.jpg', imageAlt: 'item 1 image'},
+            {id: 2, name: 'item 2', description: 'item 2 description', imagePath: '/images/placeholder.jpg', imageAlt: 'item 2 image'},
+            {id: 3, name: 'item 3', description: 'item 3 description', imagePath: '/images/placeholder.jpg', imageAlt: 'item 3 image'}
+        ]
+    }
+    res.render('items', context)
+})
+
+app.get('/items/:id', (req, res) => {
+    res.render('itemDetail')
+})
+
+app.get('/items/history', (req, res) => {
+    res.render('itemHistory')
+})
+
+app.use((req, res, next) => {
+    res.status(404)
+    res.render('404')
+})
+
+app.use((error, req, res, next) => {
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(PORT, ()=> {
+    console.log('Server started on port 3000')
+})
