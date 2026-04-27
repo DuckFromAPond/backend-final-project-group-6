08d4e3c7308a9ff48d04a0947fad7ef91ad19d91
TANGHuyming <2023277tang@aupp.edu.kh>
2026-04-27 11:45:41 +0700

    fix: image upload not working in prod

--- config/app.config.js
+++ config/app.config.js
@@ -8,8 +8,7 @@ module.exports = {
   PORT:        process.env.PORT        || 3000,
   DOMAIN:      process.env.DOMAIN      || 'localhost',
   NODE_ENV:    process.env.NODE_ENV    || 'development',
-  BASE_URL:    process.env.BASE_URL    || "http://localhost:3000",
-  PRODUCTION_URL: process.env.PRODUCTION_URL || "https://websitename.com",
+  BASE_URL:    process.env.BASE_URL    || "http://localhost:3000", // Change this to your production URL, or set BASE_URL env var
   DB_PROVIDER:  process.env.DB_PROVIDER || 'mongodb',
   SUPABASE_URL:process.env.SUPABASE_URL || 'No DB',
   SUPABASE_SERVICE_ROLE_KEY:process.env.SUPABASE_SERVICE_ROLE_KEY || 'No Key',
